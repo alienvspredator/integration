ee10bec3d81b1827cb0901432e7808d1155af09a
Danylo Shevchenko <danilshevchenko123456@gmail.com>
2019-12-17 01:16:14 +0200

    Modified euler method

--- src/Integral/Euler.hs
+++ src/Integral/Euler.hs
@@ -1,5 +1,6 @@
 module Integral.Euler
   ( integrate
+  , integrate'
   )
 where
 
@@ -11,7 +12,34 @@ eulerStep f step point@(Point x y) = Point xnew ynew
   xnew = x + step
   ynew = y + step * f point
 
-integrate :: (Point -> Double) -> Point -> Double -> Double -> [Point]
-integrate f point xf step = takeWhile (\(Point x _) -> x <= xf)
+eulerStep' :: (Point -> Double) -> Double -> Point -> Point
+eulerStep' f step point@(Point x y) = Point xnew ynew
+ where
+  xnew = x + step
+  ynew = y + step * (f point + f (Point xnew y)) / 2
+
+-- | Integrates with specified step function
+_integrate
+  -- | Step function
+  :: ((Point -> Double) -> Double -> Point -> Point)
+  -- | Function
+  -> (Point -> Double)
+  -- | Start point
+  -> Point
+  -- | x interval
+  -> Double
+  -- | Step
+  -> Double
+  -- | Points by step
+  -> [Point]
+_integrate stepFn f point xf step = takeWhile (\(Point x _) -> x <= xf)
   $ iterator point
-  where iterator = iterate $ eulerStep f step
+  where iterator = iterate $ stepFn f step
+
+-- | Integrates with Euler's method
+integrate :: (Point -> Double) -> Point -> Double -> Double -> [Point]
+integrate = _integrate eulerStep
+
+-- Integrates with modified Euler's method
+integrate' :: (Point -> Double) -> Point -> Double -> Double -> [Point]
+integrate' = _integrate eulerStep'
