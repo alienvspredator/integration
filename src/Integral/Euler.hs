37216a8cf0f6e35329100e151b71eedfa0dc39f0
Danylo Shevchenko <danilshevchenko123456@gmail.com>
2019-12-16 23:41:20 +0200

    Euler integrate method

--- src/Integral/Euler.hs
+++ src/Integral/Euler.hs
@@ -13,3 +13,8 @@ eulerStep f step point@(Point x y) = Point xnew ynew
  where
   xnew = x + step
   ynew = y + step * f point
+
+integrate :: (Point -> Double) -> Point -> Double -> Double -> [Point]
+integrate f point xf step = takeWhile (\(Point x _) -> x <= xf)
+  $ iterator point
+  where iterator = iterate $ eulerStep f step
