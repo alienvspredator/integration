97d404456bdf8f1e8c7ab3902b0e727773b2a2fc
Danylo Shevchenko <danilshevchenko123456@gmail.com>
2019-12-17 07:46:09 +0200

    fix(LeftDiff): func fix

--- src/Differentiate/LeftDiff.hs
+++ src/Differentiate/LeftDiff.hs
@@ -3,7 +3,7 @@ module Differentiate.LeftDiff
   )
 where
 
-import           Import
+import           Types
 
 leftStep :: (Point -> Double) -> Double -> Point -> Point
 leftStep f step point@(Point x y) = Point xnew ynew
@@ -11,7 +11,17 @@ leftStep f step point@(Point x y) = Point xnew ynew
   xnew = x + step
   ynew = (f point - y) / step
 
-differentiate :: (Point -> Double) -> Point -> Double -> Double -> [Point]
+differentiate :: (Double -> Double) -> Point -> Double -> Double -> [Point]
 differentiate f point xf step = takeWhile (\(Point x _) -> x <= xf)
   $ iterator point
-  where iterator = iterate $ leftStep f step
+  where iterator = iterate $ \(Point x _) -> derive f step x
+
+-- differentiate :: (Point -> Double) -> Point -> Double -> Double -> [Point]
+-- differentiate f point@(Point x y) xf step = Point (x + step) (derive step f y)
+
+-- derive :: (Fractional a) => a -> (a -> a) -> (a -> a)
+derive :: (Double -> Double) -> Double -> Double -> Point
+derive f step x = Point xnew ynew
+  where
+    xnew = x + step
+    ynew = (f xnew - f x) / step
